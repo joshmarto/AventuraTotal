b1ea8237c47123ef1fcf0b456a17c5f68cfbe31f
Josh Martinez <jmartinezolivares1@gmail.com>
2021-07-01 11:40:33 -0600

    carousel semi-finished

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-console.log('Hello there');
-$('#myCarousel').on('slid.bs.carousel', function (e) {
-  $('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+('#myCarousel').on('slid.bs.carousel', function (e) {
+  ('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
 })
 
-$('#myCarousel').carousel('1') // Will start sliding to the slide 1 and returns to the caller
-$('#myCarousel').carousel('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
+('#myCarousel').carousel('1') // Will start sliding to the slide 1 and returns to the caller
+('#myCarousel').carousel('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
